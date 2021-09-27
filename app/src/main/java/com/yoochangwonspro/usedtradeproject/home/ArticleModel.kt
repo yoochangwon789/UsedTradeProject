e380e53c1110395a3f97a4cff1cfb817decfc3f5
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-27 20:29:12 +0900

    Feat : ArticleModel 데이터 클래스 파라미터 추가

--- app/src/main/java/com/yoochangwonspro/usedtradeproject/home/ArticleModel.kt
+++ app/src/main/java/com/yoochangwonspro/usedtradeproject/home/ArticleModel.kt
@@ -1,5 +1,9 @@
 package com.yoochangwonspro.usedtradeproject.home
 
 data class ArticleModel(
-
+    val sellerId: String,
+    val title: String,
+    val createdAt: Long,
+    val price: String,
+    val imageUrl: String
 )
