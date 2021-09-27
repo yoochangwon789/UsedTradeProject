de2d7a0605c0fcb2e920fd8d18607b1208bf80fc
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-27 20:19:21 +0900

    Feat : fragment_home 레이아웃 초기 디자인 구성

--- app/src/main/java/com/yoochangwonspro/usedtradeproject/home/HomeFragment.kt
+++ app/src/main/java/com/yoochangwonspro/usedtradeproject/home/HomeFragment.kt
@@ -4,6 +4,15 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.yoochangwonspro.usedtradeproject.R
+import com.yoochangwonspro.usedtradeproject.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    private var binding: FragmentHomeBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val fragmentHomeBinding = FragmentHomeBinding.bind(view)
+        binding = fragmentHomeBinding
+    }
 }
