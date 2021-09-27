06b3fe4ad943d9f0b7d4c9a2048f6b289cb27e0b
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-27 19:28:55 +0900

    Feat : 메뉴 하단탭의 관한 Fragment 인스턴스 생성

--- app/src/main/java/com/yoochangwonspro/usedtradeproject/MainActivity.kt
+++ app/src/main/java/com/yoochangwonspro/usedtradeproject/MainActivity.kt
@@ -3,11 +3,18 @@ package com.yoochangwonspro.usedtradeproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.yoochangwonspro.usedtradeproject.chatlist.ChatListFragment
+import com.yoochangwonspro.usedtradeproject.home.HomeFragment
+import com.yoochangwonspro.usedtradeproject.mypage.MyPageFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val homeFragment = HomeFragment()
+        val chatListFragment = ChatListFragment()
+        val myPageFragment = MyPageFragment()
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
 
