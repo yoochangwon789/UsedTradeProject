2776172f50007f274150eeb8587a595580ae2145
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-27 21:42:21 +0900

    Feat : DatabaseReference 전역 프로퍼티 추가 및 초기화 작업

--- app/src/main/java/com/yoochangwonspro/usedtradeproject/home/HomeFragment.kt
+++ app/src/main/java/com/yoochangwonspro/usedtradeproject/home/HomeFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.yoochangwonspro.usedtradeproject.R
 import com.yoochangwonspro.usedtradeproject.databinding.FragmentHomeBinding
@@ -17,11 +19,14 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     private val auth: FirebaseAuth by lazy {
         Firebase.auth
     }
+    private lateinit var articleDB: DatabaseReference
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val fragmentHomeBinding = FragmentHomeBinding.bind(view)
         binding = fragmentHomeBinding
+
+        articleDB = Firebase.database.reference
 
         articleAdapter = ArticleAdapter()
         fragmentHomeBinding.articleRecyclerView.layoutManager = LinearLayoutManager(context)
