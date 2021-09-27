d1f7b6346e6e241d788d58bf6f610aacacaba664
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-27 19:27:40 +0900

    Feat : setOnItemSelectedListener 함수 초기 셋업

--- app/src/main/java/com/yoochangwonspro/usedtradeproject/MainActivity.kt
+++ app/src/main/java/com/yoochangwonspro/usedtradeproject/MainActivity.kt
@@ -2,10 +2,22 @@ package com.yoochangwonspro.usedtradeproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {}
+                R.id.chatList -> {}
+                R.id.myPage -> {}
+            }
+            true
+        }
     }
 }
