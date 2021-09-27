80484569090344fac64b8d4ea056bc18768f624e
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-27 19:55:27 +0900

    Feat : viewBinding 의존성 추가

--- app/src/main/java/com/yoochangwonspro/usedtradeproject/home/HomeFragment.kt
+++ app/src/main/java/com/yoochangwonspro/usedtradeproject/home/HomeFragment.kt
@@ -1,5 +1,7 @@
 package com.yoochangwonspro.usedtradeproject.home
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.yoochangwonspro.usedtradeproject.R
 
