f9513c2a1eb419471f293efaea983d55461eb8d6
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-28 19:09:29 +0900

    Feat : 이미지 권한 요청 임시코드 작성

--- app/src/main/java/com/yoochangwonspro/usedtradeproject/home/AddArticleActivity.kt
+++ app/src/main/java/com/yoochangwonspro/usedtradeproject/home/AddArticleActivity.kt
@@ -1,12 +1,43 @@
 package com.yoochangwonspro.usedtradeproject.home
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.yoochangwonspro.usedtradeproject.R
 
 class AddArticleActivity : AppCompatActivity() {
+
+    private val imageAddButton: Button by lazy {
+        findViewById(R.id.imageAddButton)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_article)
+
+        imageAddButton.setOnClickListener {
+            when {
+                ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                        == PackageManager.PERMISSION_GRANTED -> {
+
+                        }
+                shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE) -> {
+
+                }
+                else -> {
+
+                }
+            }
+        }
+    }
+
+    private fun navigatePhotos() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "image/*"
     }
 }
