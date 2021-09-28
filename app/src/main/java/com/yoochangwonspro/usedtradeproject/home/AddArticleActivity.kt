f5a3bc35868e35c57dd0244a7ba45981e81c9311
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-28 17:01:52 +0900

    Feat : activity_add_article 레이아웃의 상단 툴바 추가

--- app/src/main/java/com/yoochangwonspro/usedtradeproject/home/AddArticleActivity.kt
+++ app/src/main/java/com/yoochangwonspro/usedtradeproject/home/AddArticleActivity.kt
@@ -2,9 +2,11 @@ package com.yoochangwonspro.usedtradeproject.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.yoochangwonspro.usedtradeproject.R
 
 class AddArticleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_article)
     }
 }
