ed8d64d2311baea52c08712a3b419a4eac390665
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-30 02:02:31 +0900

    Feat : ChatListFragment view binding 작업

--- app/src/main/java/com/yoochangwonspro/usedtradeproject/chatlist/ChatListFragment.kt
+++ app/src/main/java/com/yoochangwonspro/usedtradeproject/chatlist/ChatListFragment.kt
@@ -1,7 +1,20 @@
 package com.yoochangwonspro.usedtradeproject.chatlist
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.yoochangwonspro.usedtradeproject.R
+import com.yoochangwonspro.usedtradeproject.databinding.FragmentChatlistBinding
 
 class ChatListFragment : Fragment(R.layout.fragment_chatlist) {
+
+    private var binding: FragmentChatlistBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val fragmentChatListBinding = FragmentChatlistBinding.bind(view)
+        binding = fragmentChatListBinding
+
+
+    }
 }
