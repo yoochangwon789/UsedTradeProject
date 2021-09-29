e9d58822be03c48c71225982f4425cd740dcdea2
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-30 00:16:38 +0900

    Feat : MyPageFragment view binding 코드 구현

--- app/src/main/java/com/yoochangwonspro/usedtradeproject/mypage/MyPageFragment.kt
+++ app/src/main/java/com/yoochangwonspro/usedtradeproject/mypage/MyPageFragment.kt
@@ -1,7 +1,18 @@
 package com.yoochangwonspro.usedtradeproject.mypage
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.yoochangwonspro.usedtradeproject.R
+import com.yoochangwonspro.usedtradeproject.databinding.FragmentMypageBinding
 
 class MyPageFragment : Fragment(R.layout.fragment_mypage) {
+
+    private var binding: FragmentMypageBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val fragmentMyPageBinding = FragmentMypageBinding.bind(view)
+        binding = fragmentMyPageBinding
+    }
 }
