099cfabc1b995d65bc4289c7047694a1616e1f0c
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-30 19:48:18 +0900

    Feat : ChatListAdapter 전역 프로퍼티 추가 및 RecyclerView Adapter 초기화

--- app/src/main/java/com/yoochangwonspro/usedtradeproject/chatlist/ChatListFragment.kt
+++ app/src/main/java/com/yoochangwonspro/usedtradeproject/chatlist/ChatListFragment.kt
@@ -3,17 +3,29 @@ package com.yoochangwonspro.usedtradeproject.chatlist
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.yoochangwonspro.usedtradeproject.R
 import com.yoochangwonspro.usedtradeproject.databinding.FragmentChatlistBinding
 
 class ChatListFragment : Fragment(R.layout.fragment_chatlist) {
 
     private var binding: FragmentChatlistBinding? = null
+    private lateinit var chatListAdapter: ChatListAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val fragmentChatListBinding = FragmentChatlistBinding.bind(view)
         binding = fragmentChatListBinding
 
+        chatListAdapter = ChatListAdapter(
+            onItemClicked = {
+
+            }
+        )
+
+        fragmentChatListBinding.chatListRecyclerView.apply {
+            adapter = chatListAdapter
+            layoutManager = LinearLayoutManager(context)
+        }
     }
 }
