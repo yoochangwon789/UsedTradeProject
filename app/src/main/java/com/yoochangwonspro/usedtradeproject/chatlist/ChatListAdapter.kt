2e2f2f327829b70829e6370081fa4ab5a737586a
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-30 19:27:33 +0900

    Add : ChatListAdapter 클래스 추가

---
+++ app/src/main/java/com/yoochangwonspro/usedtradeproject/chatlist/ChatListAdapter.kt
@@ -0,0 +1,20 @@
+package com.yoochangwonspro.usedtradeproject.chatlist
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+
+class ChatListAdapter: ListAdapter<ChatListItem, ChatListViewHolder>(diffUtil) {
+
+    inner class ChatListViewHolder() {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatListViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ChatListViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
