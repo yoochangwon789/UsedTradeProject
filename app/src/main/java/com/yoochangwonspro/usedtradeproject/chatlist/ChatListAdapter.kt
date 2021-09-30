244a09a6429fde28d21aba61a72cbbf6cb87adcb
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-30 19:34:09 +0900

    Feat : ChatListAdapter ListAdapter 초기 코드 셋업

--- app/src/main/java/com/yoochangwonspro/usedtradeproject/chatlist/ChatListAdapter.kt
+++ app/src/main/java/com/yoochangwonspro/usedtradeproject/chatlist/ChatListAdapter.kt
@@ -1,20 +1,42 @@
 package com.yoochangwonspro.usedtradeproject.chatlist
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.yoochangwonspro.usedtradeproject.databinding.ItemChatListBinding
 
-class ChatListAdapter: ListAdapter<ChatListItem, ChatListViewHolder>(diffUtil) {
+class ChatListAdapter : ListAdapter<ChatListItem, ChatListAdapter.ChatListViewHolder>(diffUtil) {
 
-    inner class ChatListViewHolder() {
+    inner class ChatListViewHolder(private val binding: ItemChatListBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
+        fun bind(chatListItem: ChatListItem) {
+
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatListViewHolder {
-        TODO("Not yet implemented")
+        return ChatListViewHolder(
+            ItemChatListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
     }
 
     override fun onBindViewHolder(holder: ChatListViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(currentList[position])
     }
 
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<ChatListItem>() {
+            override fun areItemsTheSame(oldItem: ChatListItem, newItem: ChatListItem): Boolean {
+                TODO("Not yet implemented")
+            }
+
+            override fun areContentsTheSame(oldItem: ChatListItem, newItem: ChatListItem): Boolean {
+                TODO("Not yet implemented")
+            }
+
+        }
+    }
 }
