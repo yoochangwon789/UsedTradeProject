b57f4a0ba3b2e4a344693c3d14bddb952988ba26
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-30 20:38:00 +0900

    Feat : chatDB 의 addListenerForSingleValueEvent 메서드 구현

--- app/src/main/java/com/yoochangwonspro/usedtradeproject/chatlist/ChatListFragment.kt
+++ app/src/main/java/com/yoochangwonspro/usedtradeproject/chatlist/ChatListFragment.kt
@@ -6,7 +6,14 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.yoochangwonspro.usedtradeproject.DBKey.Companion.CHILD_CHAT
+import com.yoochangwonspro.usedtradeproject.DBKey.Companion.DB_USERS
 import com.yoochangwonspro.usedtradeproject.R
 import com.yoochangwonspro.usedtradeproject.databinding.FragmentChatlistBinding
 
@@ -40,6 +47,24 @@ class ChatListFragment : Fragment(R.layout.fragment_chatlist) {
         if (auth.currentUser == null) {
             return
         }
+
+        val chatDB = Firebase.database.reference
+            .child(DB_USERS).child(auth.currentUser?.uid.orEmpty()).child(CHILD_CHAT)
+
+        chatDB.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                snapshot.children.forEach {
+                    val model = it.getValue(ChatListItem::class.java)
+                    model ?: return
+
+                    chatRoomList.add(model)
+                }
+                chatListAdapter.submitList(chatRoomList)
+                chatListAdapter.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+        })
     }
 
     override fun onResume() {
