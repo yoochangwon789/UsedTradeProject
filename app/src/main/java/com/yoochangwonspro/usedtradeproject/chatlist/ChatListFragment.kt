3e9ca21fb534ecdf12351bc4ccedad9f9850e5f3
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-09-30 19:51:01 +0900

    Feat : Firebase Auth 예외처리

--- app/src/main/java/com/yoochangwonspro/usedtradeproject/chatlist/ChatListFragment.kt
+++ app/src/main/java/com/yoochangwonspro/usedtradeproject/chatlist/ChatListFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.yoochangwonspro.usedtradeproject.R
 import com.yoochangwonspro.usedtradeproject.databinding.FragmentChatlistBinding
 
@@ -11,6 +14,10 @@ class ChatListFragment : Fragment(R.layout.fragment_chatlist) {
 
     private var binding: FragmentChatlistBinding? = null
     private lateinit var chatListAdapter: ChatListAdapter
+    private val auth: FirebaseAuth by lazy {
+        Firebase.auth
+    }
+    private val chatRoomList = mutableListOf<ChatListItem>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -19,13 +26,17 @@ class ChatListFragment : Fragment(R.layout.fragment_chatlist) {
 
         chatListAdapter = ChatListAdapter(
             onItemClicked = {
-
+                // 채팅방으로 이동하는 코드
             }
         )
 
         fragmentChatListBinding.chatListRecyclerView.apply {
             adapter = chatListAdapter
             layoutManager = LinearLayoutManager(context)
+        }
+
+        if (auth.currentUser == null) {
+            return
         }
     }
 }
