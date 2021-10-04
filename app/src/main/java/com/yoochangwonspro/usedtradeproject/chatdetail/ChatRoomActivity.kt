0b335700f79b0f7c94c3fa11d6f633d5008ad5ed
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-10-04 22:52:43 +0900

    Feat : messageEditText 전역 프로퍼티 설정 및 sendButton 클릭 리스너 부분 구현

--- app/src/main/java/com/yoochangwonspro/usedtradeproject/chatdetail/ChatRoomActivity.kt
+++ app/src/main/java/com/yoochangwonspro/usedtradeproject/chatdetail/ChatRoomActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
@@ -29,6 +30,10 @@ class ChatRoomActivity : AppCompatActivity() {
         findViewById(R.id.sendButton)
     }
 
+    private val messageEditText: EditText by lazy {
+        findViewById(R.id.messageEditText)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_room)
@@ -37,7 +42,12 @@ class ChatRoomActivity : AppCompatActivity() {
         chatRecyclerView.layoutManager = LinearLayoutManager(this)
 
         sendButton.setOnClickListener {
-
+            val chatList = auth.currentUser?.uid?.let { uid->
+                ChatItem(
+                    senderId = uid,
+                    message = messageEditText.text.toString()
+                )
+            }
         }
 
     }
