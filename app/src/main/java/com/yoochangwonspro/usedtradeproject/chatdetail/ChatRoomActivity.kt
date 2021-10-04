5e0ef9d443c9197aa6c8f16b5ccad34d274272f7
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-10-04 22:29:09 +0900

    Feat : Firebase Auth 전역 프로퍼티 추가

--- app/src/main/java/com/yoochangwonspro/usedtradeproject/chatdetail/ChatRoomActivity.kt
+++ app/src/main/java/com/yoochangwonspro/usedtradeproject/chatdetail/ChatRoomActivity.kt
@@ -2,9 +2,17 @@ package com.yoochangwonspro.usedtradeproject.chatdetail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.yoochangwonspro.usedtradeproject.R
 
 class ChatRoomActivity : AppCompatActivity() {
+
+    private val auth: FirebaseAuth by lazy {
+        Firebase.auth
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_room)
