5598118648850d9f8158988ce6f0444a220d5c84
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-10-04 22:38:23 +0900

    Add : ChatItemAdapter, item_chat 생성 및 초기 함수 셋업

---
+++ app/src/main/java/com/yoochangwonspro/usedtradeproject/chatdetail/ChatItemAdapter.kt
@@ -0,0 +1,41 @@
+package com.yoochangwonspro.usedtradeproject.chatdetail
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.yoochangwonspro.usedtradeproject.databinding.ItemChatBinding
+
+class ChatItemAdapter : ListAdapter<ChatItem, ChatItemAdapter.ViewHolder>(diffUtil) {
+
+    inner class ViewHolder(private val binding: ItemChatBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(chatItem: ChatItem) {
+
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(ItemChatBinding.inflate(LayoutInflater.from(parent.context),
+            parent,
+            false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<ChatItem>() {
+            override fun areItemsTheSame(oldItem: ChatItem, newItem: ChatItem): Boolean {
+                return oldItem.senderId == newItem.senderId
+            }
+
+            override fun areContentsTheSame(oldItem: ChatItem, newItem: ChatItem): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
