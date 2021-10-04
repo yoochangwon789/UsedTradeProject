88f71d1401a6c40f94b95d997c14fb26f13b5704
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-10-04 23:43:14 +0900

    Feat : chatDB 의 관한 코드 구현

--- app/src/main/java/com/yoochangwonspro/usedtradeproject/chatdetail/ChatRoomActivity.kt
+++ app/src/main/java/com/yoochangwonspro/usedtradeproject/chatdetail/ChatRoomActivity.kt
@@ -9,7 +9,13 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.yoochangwonspro.usedtradeproject.DBKey.Companion.DB_CHATS
 import com.yoochangwonspro.usedtradeproject.R
 
 class ChatRoomActivity : AppCompatActivity() {
@@ -34,20 +40,47 @@ class ChatRoomActivity : AppCompatActivity() {
         findViewById(R.id.messageEditText)
     }
 
+    private var chatDB: DatabaseReference? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_room)
+
+
+        val chatKey = intent.getLongExtra("chetKey", -1)
+        chatDB = Firebase.database.reference.child(DB_CHATS).child("$chatKey")
+        chatDB?.addChildEventListener(object : ChildEventListener {
+            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+                val chatItem = snapshot.getValue(ChatItem::class.java)
+                chatItem ?: return
+
+                chatList.add(chatItem)
+                adapter.submitList(chatList)
+                adapter.notifyDataSetChanged()
+            }
+
+            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {}
+
+            override fun onChildRemoved(snapshot: DataSnapshot) {}
+
+            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
+
+            override fun onCancelled(error: DatabaseError) {}
+
+        })
 
         chatRecyclerView.adapter = adapter
         chatRecyclerView.layoutManager = LinearLayoutManager(this)
 
         sendButton.setOnClickListener {
-            val chatList = auth.currentUser?.uid?.let { uid->
+            val chatItem = auth.currentUser?.uid?.let { uid->
                 ChatItem(
                     senderId = uid,
                     message = messageEditText.text.toString()
                 )
             }
+
+            chatDB?.push()?.setValue(chatItem)
         }
 
     }
