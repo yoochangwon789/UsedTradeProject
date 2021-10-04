e676e813d6275fbeae04a1e1d25252645cb69d31
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-10-04 22:50:00 +0900

    Feat : chatRecyclerView, sendButton 전역 프로퍼티 생성 및 초기 함수 셋업

--- app/src/main/java/com/yoochangwonspro/usedtradeproject/chatdetail/ChatRoomActivity.kt
+++ app/src/main/java/com/yoochangwonspro/usedtradeproject/chatdetail/ChatRoomActivity.kt
@@ -1,7 +1,11 @@
 package com.yoochangwonspro.usedtradeproject.chatdetail
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -17,8 +21,24 @@ class ChatRoomActivity : AppCompatActivity() {
 
     private val adapter = ChatItemAdapter()
 
+    private val chatRecyclerView: RecyclerView by lazy {
+        findViewById(R.id.chatRecyclerView)
+    }
+
+    private val sendButton: Button by lazy {
+        findViewById(R.id.sendButton)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat_room)
+
+        chatRecyclerView.adapter = adapter
+        chatRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        sendButton.setOnClickListener {
+
+        }
+
     }
 }
